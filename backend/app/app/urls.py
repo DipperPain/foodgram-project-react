9345bc3b780a18af37049fca5de905079d1fb400
DipperPain <flexalexanimal@gmail.com>
2022-05-12 13:36:25 +0300

    3 12.05.2022 add docker

--- backend/app/app/urls.py
+++ backend/app/app/urls.py
@@ -19,7 +19,6 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
     path('api/', include('api.urls')),
