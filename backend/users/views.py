76cdf9569fee687a2994c6158d12daed79d0e1a6
DipperPain <flexalexanimal@gmail.com>
2022-05-16 01:18:07 +0300

    ready

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,5 +1,8 @@
 from djoser.views import UserViewSet
-from .serializers import RegistrationSerializer
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from .serializers import RegistrationSerializer, UserSerializer
 from users.models import User
 
 
@@ -9,3 +12,19 @@ class UserViewSet(UserViewSet):
 
     def get_queryset(self):
         return User.objects.all()
+
+    @action(
+        methods=['patch', 'get'],
+        permission_classes=[IsAuthenticated],
+        detail=False,
+        url_path='me',
+        url_name='me',
+    )
+    def me(self, request, *args, **kwargs):
+        user = self.request.user
+        serializer = UserSerializer(user)
+        if self.request.method == 'PATCH' and user.role != 'user':
+            serializer = UserSerializer(user, data=request.data, partial=True)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+        return Response(serializer.data)
