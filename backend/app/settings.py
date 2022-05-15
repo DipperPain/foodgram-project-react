0fcb0fb1c30447e6500622e045e4bb6f1b2367ce
DipperPain <flexalexanimal@gmail.com>
2022-05-15 20:32:47 +0300

    ready

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -111,7 +111,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql_psycopg2'),
+        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
         'NAME': os.getenv('DB_NAME', 'postgres'),
         'USER': os.getenv('POSTGRES_USER', 'postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
