20d549d96dfd22776393f3a77d5bda56a66e22e5
DipperPain <flexalexanimal@gmail.com>
2022-05-20 00:20:46 +0300

    ready

--- backend/api/views.py
+++ backend/api/views.py
@@ -48,7 +48,6 @@ class RecipeViewSet(viewsets.ModelViewSet):
         serializer = serializers(data=data, context={'request': request})
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.data)
 
     @staticmethod
     def delete_method_for_actions(request, pk, model):
