3da465d02f188844645d7ffa8d5c48e424c012da
DipperPain <flexalexanimal@gmail.com>
2022-05-20 13:57:39 +0300

    r1

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'api',
     'users',
     'recipes',
+    'debug_toolbar'
 
 ]
 
@@ -40,6 +41,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'app.urls'
--- backend/app/urls.py
+++ backend/app/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
     path('api/', include('api.urls')),
+    path('__debug__/', include('debug_toolbar.urls')),
 ]
 urlpatterns += static(
     settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
