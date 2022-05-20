87aaa32c3ef820fd35a813cb6c56391b2b2da91e
DipperPain <flexalexanimal@gmail.com>
2022-05-20 18:08:55 +0300

    ready

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -144,21 +144,3 @@ DJOSER = {
     },
     'HIDE_USERS': False,
 }
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': '/debug.log',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    },
-}
