b7f5e36c55f06c2bc00d39e66656e34ace08fc21
DipperPain <flexalexanimal@gmail.com>
2022-05-20 14:14:42 +0300

    r1

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'cvo5p#n%=y*()l%lzz8&ex(bbdi!jdj!6r9j5*%q72lfx+p!-b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
