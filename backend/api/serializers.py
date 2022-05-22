7031f309c9361f86ddb6c1c8b8b14454f836ef0b
DipperPain <flexalexanimal@gmail.com>
2022-05-22 14:06:22 +0300

    r1

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -103,12 +103,11 @@ class RecipePostSerializer(serializers.ModelSerializer):
                   'image', 'name', 'text', 'cooking_time')
 
     @staticmethod
-    def create_ingredients_tags(recipe, ingredients, tags, amount):
+    def create_ingredients_tags(recipe, ingredients, tags):
         for ingredient in ingredients:
             AmountIngredientForRecipe.objects.create(
                 recipe=recipe,
                 ingredient=ingredient['id'],
-                amount=amount
             )
         for tag in tags:
             recipe.tags.add(tag)
@@ -116,12 +115,11 @@ class RecipePostSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         ingredients = validated_data.pop('ingredients')
         tags = validated_data.pop('tags')
-        amount = validated_data.pop('amount')
         recipe = Recipe.objects.create(
             author=self.context.get('request').user,
             **validated_data
         )
-        self.create_ingredients_tags(recipe, ingredients, tags, amount)
+        self.create_ingredients_tags(recipe, ingredients, tags)
         return recipe
 
     def update(self, recipe, validated_data):
