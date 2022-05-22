9ba4e71cdc0b41f6f04e7a7cf1fc54b86719c5f2
DipperPain <flexalexanimal@gmail.com>
2022-05-22 11:42:12 +0300

    r1

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -32,8 +32,7 @@ class AmountIngredientForRecipeGetSerializer(serializers.ModelSerializer):
 
 
 class AmountIngredientForRecipePostSerializer(serializers.ModelSerializer):
-    id = serializers.PrimaryKeyRelatedField(
-    queryset=Ingredient.objects.all(), many=True)
+    id = serializers.PrimaryKeyRelatedField(queryset=Ingredient.objects.all())
     amount = serializers.IntegerField(min_value=1)
 
     class Meta:
