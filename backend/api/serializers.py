b7f5ae8c0594f91fddfb658dbe7058140d6ef1e2
DipperPain <flexalexanimal@gmail.com>
2022-05-23 00:41:07 +0300

    r1

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -110,7 +110,7 @@ class RecipePostSerializer(serializers.ModelSerializer):
                 amount=ingredient['amount'],
                 recipe=Recipe.objects.get(id=recipe.id)
             )
-            recipe.ingredients.add(ingredient)
+
         for tag in tags:
             recipe.tags.add(tag)
 
