0be82eaf34ed4f1b3e448748e5c123bbdf12439e
DipperPain <flexalexanimal@gmail.com>
2022-05-23 14:34:36 +0300

    r1

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -105,7 +105,7 @@ class RecipePostSerializer(serializers.ModelSerializer):
     @staticmethod
     def create_ingredients_tags(recipe, ingredients, tags):
         for ingredient in ingredients:
-            amount = int(ingredient['amount'])
+            amount = int(ingredient['amount'].all())
             AmountIngredientForRecipe.objects.create(
                 ingredient=ingredient['id'],
                 amount=amount,
