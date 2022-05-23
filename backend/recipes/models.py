50b8eec7076984f797725cb73ea329c86b2143c1
DipperPain <flexalexanimal@gmail.com>
2022-05-23 13:42:24 +0300

    r1

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -77,11 +77,6 @@ class Recipe(models.Model):
 
 
 class AmountIngredientForRecipe(models.Model):
-    recipe = models.ForeignKey(
-        Recipe,
-        on_delete=models.CASCADE,
-        verbose_name='Рецепт'
-    )
     ingredient = models.ForeignKey(
         Ingredient,
         on_delete=models.CASCADE,
