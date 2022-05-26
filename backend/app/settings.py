a65343e02d3da3fa52ac07327c7abd8ebdd2c528
DipperPain <flexalexanimal@gmail.com>
2022-05-26 21:21:10 +0300

    r

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 STATIC_URL = '/backend_static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'backend_static')
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/backend_media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'backend_media')
 
 CORS_ORIGIN_ALLOW_ALL = True
